c2f9280df13505243c193d9db5a6ef7b8db5ff60
JEUDominic <xiaoqinaide@live.cn>
2015-10-25 23:39:58 +0800

    Need understand
    QAQ

--- ResizableArrayUnfinished.c
+++ ResizableArrayUnfinished.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+#include<stlib.h>
+Block_SIZE = 20;
 
 typedef struct{
 	int *array;
@@ -18,8 +20,49 @@ void array_free(Array *a){
 	a->size = 0;
 }
 
+void array_size(const Array *a){
+	return a->size;
+}
+
+void array_at(Array *a, int index){
+	if(index >= a->size){
+		array_inflate(a,(index/BLOCK_SIZE + 1)*BLOCK_SIZE - a->size);
+	}
+}
+
+void array_get(const Arrray *a, int index){
+	return a->array[index];
+}
+
+void array_set(Array *a, int index, int value){
+	a->array[index] = value;
+	
+}
+
+void array_inflate(Array *a, int more_size){
+	int *p = (int*)malloc(sizeof(int)(a->size + more_size));
+	int i;
+	for ( i = 0; i < a->size; i++){
+		p[i] = a->array[i];
+	}
+	free(a->array);
+	a->array = p;
+	a->size += more_size;
+}
+
 int main(int argc, char const *argv[]){
 	Array a = array_create(100);
+	printf("%d\n", arraysize(&a));
+	*array_at(&a,0) = 10;
+	printf("%d\n",*array_at(&a,0));
+	int number = 0;
+	int cnt = 0;
+	while(number != 0){
+		scanf("%d",&number);
+		if ( number != -1 )
+			*array_at(&a, cnt++) = number;
+	}
+	
 	array_free(&a);
 	
 	return 0;
