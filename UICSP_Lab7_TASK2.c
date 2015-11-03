936e2cd6bc98d83072d35c974cbb9c80fe1c5a71
JEUDominic <xiaoqinaide@live.cn>
2015-11-03 22:36:02 +0800

    Re
    The wrong program I build before.

--- UICSP_Lab7_TASK2.c
+++ UICSP_Lab7_TASK2.c
@@ -1,11 +1,12 @@
 // Code by Alicia.20151030
-
+// Recode by Alicia.20151102 Add Comments.
 #include<stdio.h>
-void exchange(float a[], int n);//Function prototype...
+char* exchange(float a[], int n);//Function prototype...
 
 int main(){
 	int i = 0;
 	float value[50]={};
+    float returnValue[2];
 	printf("Please input the values you want to do comparation(end with -1):\n");
 	//Ge the input and store in array of value...
 	do {
@@ -16,19 +17,25 @@ int main(){
 		i++;
 	}while(1);
 	//Do the bubble sorting.... 
-	exchange(value,i);
-	
+	returnValue = exchange(value,i);
+  
 	//Printf the max&min by using array of value...    
 	printf("The min is %f\n",value[0]);
+    printf("The min from return value is %f\n",returnValue[0]);
 	printf("The max is %f\n",value[i-1]);
+    printf("The max from return value is %f\n",returnValue[i-1]);
+    
+    return 0;
 
 }
 
-void exchange(float a[], int n){
+//Try to return array to main funtion...
+char* exchange(float a[], int n){
 	float temp;
+    float returnArray[2];
 	int i, j;
 	//....................Bubble Sorting................
-    for (j = 0; j < n - 1; j++){
+    for (j = 0; j < n - 1; j++){//For n numbers, do this comparation n-1 times...
         for (i = 0; i < n - 1 - j; i++){
             if(a[i] > a[i + 1]){
                 temp = a[i];
@@ -37,6 +44,10 @@ void exchange(float a[], int n){
             }
         }
 	}
+    returnArray[0] = a[0];
+    returnArray[1] = a[1];
+    
+    return &returnArray;
 
 }
 
