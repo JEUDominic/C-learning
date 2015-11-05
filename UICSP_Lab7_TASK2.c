02b7f676b3f5d6993b48f413e5a73e167ef9f06b
JEUDominic <xiaoqinaide@live.cn>
2015-11-05 23:29:03 +0800

    Return array from function

--- UICSP_Lab7_TASK2.c
+++ UICSP_Lab7_TASK2.c
@@ -1,12 +1,12 @@
 // Code by Alicia.20151030
 // Recode by Alicia.20151102 Add Comments.
 #include<stdio.h>
-char* exchange(float a[], int n);//Function prototype...
+float* exchange(float a[], int n);//Function prototype...
 
 int main(){
 	int i = 0;
 	float value[50]={};
-    float returnValue[2];
+    float *returnValue;
 	printf("Please input the values you want to do comparation(end with -1):\n");
 	//Ge the input and store in array of value...
 	do {
@@ -16,23 +16,25 @@ int main(){
 		}
 		i++;
 	}while(1);
-	//Do the bubble sorting.... 
+	// Do the bubble sorting.... 
 	returnValue = exchange(value,i);
+	free(returnArray);
+	printf("The returnArray is %f\n",returnArray[0]);
   
 	//Printf the max&min by using array of value...    
 	printf("The min is %f\n",value[0]);
     printf("The min from return value is %f\n",returnValue[0]);
 	printf("The max is %f\n",value[i-1]);
-    printf("The max from return value is %f\n",returnValue[i-1]);
+    printf("The max from return value is %f\n",returnValue[1]);
     
     return 0;
 
 }
 
 //Try to return array to main funtion...
-char* exchange(float a[], int n){
+float* exchange(float a[], int n){
 	float temp;
-    float returnArray[2];
+    static float returnArray[2];// How to free this memory..
 	int i, j;
 	//....................Bubble Sorting................
     for (j = 0; j < n - 1; j++){//For n numbers, do this comparation n-1 times...
@@ -45,10 +47,9 @@ char* exchange(float a[], int n){
         }
 	}
     returnArray[0] = a[0];
-    returnArray[1] = a[1];
+    returnArray[1] = a[n-1];
     
-    return &returnArray;
-
+    return returnArray;
 }
 
 
