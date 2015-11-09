64d55a10a7ef5404a0ba909fab30103df04b0fd5
JEUDominic <xiaoqinaide@live.cn>
2015-11-09 22:28:13 +0800

    Still need optimization

--- UICSP_Assignment3_TASK3.c
+++ UICSP_Assignment3_TASK3.c
@@ -2,38 +2,50 @@
 //  TASK3
 //  Assignment3
 //
-//  Created by Alicia.20151106
-
+//  Created by JEU.20151106
+//  Recode by JEU.20151110
 
 #include<stdio.h>
 
-int whetherPrime(int integer);
-
+int prime(unsigned int integer);
+int biggestDivisor(unsigned int integer);
 int main(){
-	unsigned int p;// Positive number...
-	int divisor;
-	printf("Please input a positive number:");
-	scanf("%d",&p);
-
-	// Getting the value that could check its prime...
-	divisor = whetherPrime(p);
-
-	if(divisor == 1)
-		printf("%d is a prime number.\n",p);
-	else
-		printf("%d is not prime number, and its biggest divisor is %d.\n",p,divisor);
-	
+    unsigned int p;// Positive number...
+    int checkValue;// Stored the return value from prime funtion...
+    int divisor;
+    printf("Please input a positive number:");
+    scanf("%d",&p);
+    
+    // Getting the value that could check its prime...
+    checkValue = prime(p);
+    
+    
+    if(checkValue == 0)
+        printf("%d is a prime number.\n",p);
+    else{
+        divisor = biggestDivisor(p);
+        printf("%d is not prime number, and its biggest divisor is %d.\n",p,divisor);
+    }
 }
 
-int whetherPrime(int integer){
-	int i;
-	int divisor;
+int prime(unsigned int integer){
+    int i;
+    
+    // Looking through
+    for(i = integer - 1;i >= integer/2;i--){
+        if(integer % i == 0)
+            return 1;
+    }
+    
+    return 0;
+}
 
-	// Get the biggest divisor except itself...
-	for(i = 1;i < integer/3;i++){
-		if(integer%i == 0)
-			divisor = i;
-	}
-
-	return divisor;
+int biggestDivisor(unsigned int integer){
+    int i;
+    int divisor;
+    for(i = 2;i < integer; i++){
+        if(integer % i == 0)
+            return integer / i;
+        
+    }
 }
